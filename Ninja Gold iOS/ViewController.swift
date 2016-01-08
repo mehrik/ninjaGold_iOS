9be033c9eb01b4139e312ce21fb3f5c0254be855
mehrik <sobreo.m@gmail.com>
2016-01-08 14:50:38 -0800

    First Commit

--- Ninja Gold iOS/ViewController.swift
+++ Ninja Gold iOS/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var goldEarnedLabel: UILabel!
+    @IBOutlet weak var goldEarnedFromFarmLabel: UILabel!
+    @IBOutlet weak var goldEarnedFromCaveLabel: UILabel!
+    @IBOutlet weak var goldEarnedFromHouseLabel: UILabel!
+    @IBOutlet weak var goldEarnedFromCasinoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        hideAllGoldEarnedButtonLabels()
+        goldEarnedLabel.text = String(0)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buildingButtonPressed(sender: UIButton) {
+        let goldEarned: Int
+        let farm = 1
+        let cave = 2
+        let house = 3
+        // let casino = 4
+        hideAllGoldEarnedButtonLabels()
+        if sender.tag == farm {
+            goldEarned = Int(arc4random_uniform(11) + 10)
+            goldEarnedFromFarmLabel.text = generateAmountGoldEarnedText(goldEarned)
+            goldEarnedFromFarmLabel.hidden = false
+        } else if sender.tag == cave {
+            goldEarned = Int(arc4random_uniform(6) + 5)
+            goldEarnedFromCaveLabel.text = generateAmountGoldEarnedText(goldEarned)
+            goldEarnedFromCaveLabel.hidden = false
+        } else if sender.tag == house {
+            goldEarned = Int(arc4random_uniform(4) + 2)
+            goldEarnedFromHouseLabel.text = generateAmountGoldEarnedText(goldEarned)
+            goldEarnedFromHouseLabel.hidden = false
+            
+        } else { //if sender.tag == casino {
+            goldEarned = Int(arc4random_uniform(101)) - 50
+            goldEarnedFromCasinoLabel.text = generateAmountGoldEarnedText(goldEarned)
+            goldEarnedFromCasinoLabel.hidden = false
+        }
+        goldEarnedLabel.text = String(goldEarned + Int(goldEarnedLabel.text!)!)
+    }
 
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        goldEarnedLabel.text = String(0)
+    }
+    
+    func hideAllGoldEarnedButtonLabels() {
+        goldEarnedFromFarmLabel.hidden = true
+        goldEarnedFromCaveLabel.hidden = true
+        goldEarnedFromHouseLabel.hidden = true
+        goldEarnedFromCasinoLabel.hidden = true
+    }
+    
+    func generateAmountGoldEarnedText(gold: Int) -> String {
+        if gold > 0 {
+            return "You earned \(gold) golds"
+        } else if gold == 0 {
+            return "You did not earn any gold!"
+        } else {
+            return "You lost \(abs(gold)) golds"
+        }
+    }
 }
 
